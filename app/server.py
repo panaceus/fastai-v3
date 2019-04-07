db35c6a2e391b4e7d721528d760ec612003dec4d
panaceus <panaceus@users.noreply.github.com>
2019-04-07 18:19:49 -0400

    Updated Dropbox link to export.pkl

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/7qkrwa5le6uth03/stage-3-50%20%28validated%29.pth?dl=1'
+export_file_url = 'https://www.dropbox.com/s/za4t9mda9slpuzg/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['Bishop', 'King', 'Knight', 'Pawn', 'Queen', 'Rook']
